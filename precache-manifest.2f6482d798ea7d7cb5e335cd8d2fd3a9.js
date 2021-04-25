04f7e17fd4bd34989f121eeac4fe7e655f56af24
cloudshare360 <cloud.share360@gmail.com>
2021-04-25 13:27:20 -0500

    Updates

---
+++ precache-manifest.2f6482d798ea7d7cb5e335cd8d2fd3a9.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a62015076fd66fc708977b95854d26d4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "585cd852250b48c53031",
+    "url": "/static/css/main.f568d28f.chunk.css"
+  },
+  {
+    "revision": "422e2a930c24b2050a36",
+    "url": "/static/js/2.02a06037.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.02a06037.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "585cd852250b48c53031",
+    "url": "/static/js/main.dd42a144.chunk.js"
+  },
+  {
+    "revision": "5dcc4a2d51d1022f96e5",
+    "url": "/static/js/runtime-main.edb82d6c.js"
+  },
+  {
+    "revision": "d5124330fa985dca973d302115a9f83e",
+    "url": "/static/media/banner_img12.d5124330.jpg"
+  },
+  {
+    "revision": "560bcba017599ced51dd88a42ee4893a",
+    "url": "/static/media/banner_img22.560bcba0.jpg"
+  },
+  {
+    "revision": "21b99d53858a6bc3d8bc63fe4dc1ec67",
+    "url": "/static/media/banner_img33.21b99d53.jpg"
+  },
+  {
+    "revision": "870581a0a206b644095cc9f4dcf3ed7a",
+    "url": "/static/media/banner_img44.870581a0.jpg"
+  }
+]);
